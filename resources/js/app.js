e8d7127bbb18516306be11852f9f8cb7265d3e59
Joaovbf <j.vitor.barros@hotmail.com>
2020-12-30 17:03:09 -0300

    Deleção de código com engenhocas, e preparação para implementar a autenticação usando Sanctum

--- resources/js/app.js
+++ resources/js/app.js
@@ -24,10 +24,6 @@ Vue.mixin({
  * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
  */
 
-// const files = require.context('./', true, /\.vue$/i)
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default))
-
-Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 Vue.component('blog', require('./components/Blog.vue').default);
 
 
